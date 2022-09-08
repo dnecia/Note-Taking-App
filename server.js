2621e723ffbb6d78ce51773791ff74a0ec0ae849
Donniecia  Corke <donnieciamc@gmail.com>
2022-09-08 19:24:45 -0400

    function to add new note

--- server.js
+++ server.js
@@ -25,3 +25,26 @@ app.get("/", (req, res) => {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
+
+//adding a function to create a note
+function creatingNewNote(body, notesArray) {
+  const newNote = body;
+  if (!Array.isArray(notesArray)) notesArray = [];
+
+  if (notesArray.length === 0) notesArray.push(0);
+
+  body.id = notesArray[0];
+  notesArray[0]++;
+
+  notesArray.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(notesArray, null, 2)
+  );
+  return newNote;
+}
+//deleting the note function
+//adding a listner
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
