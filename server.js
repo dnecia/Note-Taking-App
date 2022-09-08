5fa73c5cf3621bbd2b2e63dcadffd086db9d8c25
Donniecia  Corke <donnieciamc@gmail.com>
2022-09-08 19:32:15 -0400

    adding heroku

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 //adding npm package
+const PORT = process.env.PORT || 3001;
 const express = require("express");
 const app = express();
 const fs = require("fs");
-const PORT = process.env.PORT || 3001;
+
 const path = require("path");
 
 const theNotes = require("./db/db.json");
