e6a418fd2bb525df0054689535fcc3b420b95d8e
Donniecia  Corke <donnieciamc@gmail.com>
2022-09-08 19:07:06 -0400

    adding the API routes

--- server.js
+++ server.js
@@ -4,3 +4,24 @@ const app = express();
 const fs = require("fs");
 const PORT = process.env.PORT || 3001;
 const path = require("path");
+
+const theNotes = require("./db/db.json");
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//getting the API's
+app.get("/api/notes", (req, res) => {
+  res.json(theNotes.slice(1));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
