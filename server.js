a4d24ab6e367107c72dc9d1cdaae4f3ccf48eeee
Donniecia  Corke <donnieciamc@gmail.com>
2022-09-08 22:06:44 -0400

    heroku deploymnet

--- server.js
+++ server.js
@@ -17,12 +17,14 @@ app.get("/api/notes", (req, res) => {
   res.json(theNotes.slice(1));
 });
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
